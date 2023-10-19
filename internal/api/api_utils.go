cc3b253ba9895aad794cb3dc8f6876d925d41c4d
BorMagnus <magnus.borseth@gmail.com>
2023-10-19 18:24:03 +0200

    Refactor code and transition to JSON for flashcard data
    - Changed structure in project and renamed folders
    - Separate logic from `flashcards` into `api` and `filehandler``
    - Transition flashcard data format to JSON

---
+++ internal/api/api_utils.go
@@ -0,0 +1,90 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type Response struct {
+	Model              string `json:"model"`
+	CreatedAt          string `json:"created_at"`
+	APIResponse        string `json:"response"`
+	Done               bool   `json:"done"`
+	TotalDuration      int64  `json:"total_duration"`
+	LoadDuration       int64  `json:"load_duration"`
+	SampleCount        int    `json:"sample_count"`
+	SampleDuration     int64  `json:"sample_duration"`
+	PromptEvalCount    int    `json:"prompt_eval_count"`
+	PromptEvalDuration int64  `json:"prompt_eval_duration"`
+	EvalCount          int    `json:"eval_count"`
+	EvalDuration       int64  `json:"eval_duration"`
+	Context            []int  `json:"context"`
+}
+
+type FlashcardsResponse struct {
+	Flashcards []Flashcard `json:"flashcards"`
+}
+
+type Flashcard struct {
+	Question string `json:"question"`
+	Answer   string `json:"answer"`
+}
+
+// NewClient returns a configured HTTP client.
+func NewClient() *http.Client {
+	return &http.Client{
+		Timeout: time.Second * 10, // for example
+	}
+}
+
+// ParseAPIResponse extracts relevant data from the API response.
+func ParseAPIResponse(apiResponse string) (FlashcardsResponse, error) {
+	var fullAPIResponse string
+	var flashcardsResponse FlashcardsResponse
+
+	responses := strings.Split(apiResponse, "\n")
+	for _, response := range responses {
+		if response == "" {
+			continue
+		}
+		var apiResponseObj Response
+		err := json.Unmarshal([]byte(response), &apiResponseObj)
+		if err != nil {
+			return flashcardsResponse, fmt.Errorf("Error unmarshaling response: %w", err)
+		} else {
+			fullAPIResponse += apiResponseObj.APIResponse
+		}
+	}
+
+	startIdx := strings.Index(fullAPIResponse, "{")
+	endIdx := strings.LastIndex(fullAPIResponse, "}") + 1
+
+	if startIdx == -1 || endIdx == -1 {
+		return flashcardsResponse, fmt.Errorf("Valid JSON not found in the API response")
+	}
+
+	jsonPart := fullAPIResponse[startIdx:endIdx]
+
+	err := json.Unmarshal([]byte(jsonPart), &flashcardsResponse)
+	if err != nil {
+		return flashcardsResponse, fmt.Errorf("Error parsing flashcards JSON: %w", err)
+	}
+
+	return flashcardsResponse, nil
+}
+
+// FetchAPIResponse makes the API call and returns the body.
+func FetchAPIResponse(client *http.Client, url string, jsonData []byte) ([]byte, error) {
+	resp, err := client.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
+}
---
+++ internal/filehandler/file_utils.go
@@ -0,0 +1,98 @@
+package filehandler
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"goLlamaFlash/internal/flashcards"
+)
+
+// Processes either a file or a directory path for flashcard generation
+func ProcessPath(path string) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	switch mode := fileInfo.Mode(); {
+	case mode.IsDir():
+		visitFolder(path)
+	case mode.IsRegular():
+		handleFile(path)
+	}
+}
+
+// Function to find all files within directories
+func visitFolder(folderPath string) {
+	folder, err := os.ReadDir(folderPath)
+	if err != nil {
+		fmt.Println("Read directory error:", err)
+		return
+	}
+
+	for _, entry := range folder {
+		newPath := filepath.Join(folderPath, entry.Name())
+		if entry.IsDir() {
+			visitFolder(newPath)
+		} else if strings.HasSuffix(entry.Name(), ".md") {
+			handleFile(newPath)
+		}
+	}
+}
+
+// Handle a single markdown file
+func handleFile(filePath string) error {
+	if hasExistingFlashcards(filePath) {
+		fmt.Println("Skipping, as it already has flashcards:", filePath)
+		return nil
+	}
+	flashcards, err := flashcards.GenerateFlashcards(filePath)
+	if err != nil {
+		fmt.Println(flashcards, err)
+		return err
+	} else {
+		appendToFile(filePath, flashcards)
+		return err
+	}
+}
+
+func hasExistingFlashcards(filePath string) bool { // TODO: Move to file_utils.go
+	file, err := os.Open(filePath)
+	if err != nil {
+		println("File open error:", err)
+		return false
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "## Flashcards") {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Add the flashcards to the file
+func appendToFile(filePath string, flashcards string) {
+	// Open the file in append mode
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("File open error:", err)
+		return
+	}
+	defer file.Close()
+
+	// Add a separator and the flashcard content
+	_, err = file.WriteString("\n\n---\n## Flashcards\n```Json\n" + flashcards + "\n```")
+	if err != nil {
+		fmt.Println("File write error:", err)
+		return
+	}
+}
