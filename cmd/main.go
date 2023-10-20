10159915b04b0ead13d213790527f029a9100e7b
BorMagnus <magnus.borseth@gmail.com>
2023-10-20 16:50:06 +0200

    Removed TODOs

--- cmd/main.go
+++ cmd/main.go
@@ -19,11 +19,8 @@ func main() {
 	filehandler.ProcessPath(path)
 }
 
-// TODO: Improve on promt or change settings for the model
 // TODO: Fix error handeling
 // TODO: Create a folder scanner that checks if a there are some changes on the markdown files in the folder
 // TODO: Change the prompt to see if there is any flashcards that can be made
-// TODO: Improve HTTP Client
 // TODO: Check diff between md files
 // TODO: Save md file with SQLite?
-// TODO: Make model return a JSON with flashcards
