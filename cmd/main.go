21bc36b924b8a6b4c6fe3625861e648f7ee40f38
BorMagnus <magnus.borseth@gmail.com>
2023-10-27 12:26:33 +0200

    Modify main.go and update go.mod

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"goLlamaFlash/internal/filehandler"
-)
+import "goLlamaFlash/ui/cli"
 
 func main() {
-	var path string
-	flag.StringVar(&path, "path", "", "Path to the file or directory")
-	flag.Parse()
-
-	if path == "" {
-		fmt.Println("No path provided. Run: go run main.go --path your_file_name")
-		return
-	}
-
-	filehandler.ProcessPath(path)
+	cli.Run()
 }
-
-// TODO: Fix error handeling
-// TODO: Create a folder scanner that checks if a there are some changes on the markdown files in the folder
-// TODO: Change the prompt to see if there is any flashcards that can be made
-// TODO: Check diff between md files
-// TODO: Save md file with SQLite?
